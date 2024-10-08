67ff05942605591bef23be741c0f8aa724a99aa3
Giorgobiani2002 <160760301+Giorgobiani2002@users.noreply.github.com>
2024-10-08 20:49:12 +0400

    added/todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, { useState } from "react";
 
 function App() {
+  const [todo, setTodo] = useState("");
+  const [todoos, setToodos] = useState([]);
+  const [todos, setTodos] = useState(false);
+
+  const handleInput = (e) => {
+    setTodo(e.target.value);
+    console.log(todo);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTodos(true);
+    setToodos([...todo, todoos]);
+    
+  };
+  const remove = () => {
+    setTodos(false);
+  };
+
   return (
     <>
-      <h1 className="text-3xl font-bold text-center">Midterm 2</h1>
+      <div>
+        <form
+          className="flex justify-center items-center"
+          type="submit"
+          action=""
+        >
+          <input
+            className="border-black border-2 "
+            type="text"
+            placeholder="Search..."
+            onChange={handleInput}
+          />
+          <button onClick={handleSubmit}>ADD</button>
+        </form>
+        <div className="flex">
+          {todos ? <li>{todo}</li> : null}
+          {todos ? (
+            <button className="ml-4" onClick={remove}>
+              Remove
+            </button>
+          ) : null}
+        </div>
+      </div>
     </>
   );
 }
